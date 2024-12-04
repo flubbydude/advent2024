10639220f11f30a363774e33cf2c6c7c2b810d9b
Stuart Lustig <stueyrama@gmail.com>
2024-12-03 20:46:31 -0500

    +template

---
+++ template.rs
@@ -0,0 +1,45 @@
+fn parse_input(input: &str) -> &str {
+    input
+}
+
+fn part1(input: &str) -> usize {
+    todo!()
+}
+
+fn part2(input: &str) -> usize {
+    todo!()
+}
+
+fn main() {
+    let file_contents = std::fs::read("input.txt").unwrap();
+    let file_contents_as_str = std::str::from_utf8(&file_contents).unwrap();
+
+    let input = parse_input(file_contents_as_str);
+
+    println!("{}", part1(input));
+    println!("{}", part2(input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "";
+
+    #[test]
+    fn test_parse_input() {
+        todo!()
+    }
+
+    #[test]
+    fn test_part1() {
+        let input = parse_input(TEST_INPUT);
+        assert_eq!(0, part1(input))
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = parse_input(TEST_INPUT);
+        assert_eq!(0, part2(input))
+    }
+}
