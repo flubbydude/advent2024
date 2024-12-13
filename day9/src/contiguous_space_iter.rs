80c2fb203242adab0688f37b58be520f0c15fcbf
Stuart Lustig <stueyrama@gmail.com>
2024-12-12 20:22:07 -0500

    day9 but i cri

---
+++ day9/src/contiguous_space_iter.rs
@@ -0,0 +1,69 @@
+#[derive(Debug)]
+pub enum ContiguousDiskSpace {
+    File(DiskFile),
+    EmptySpace(EmptySpace),
+}
+
+#[derive(Debug)]
+pub struct DiskFile {
+    pub id: usize,
+    pub length: usize,
+}
+
+#[derive(Debug)]
+pub struct EmptySpace {
+    pub length: usize,
+}
+
+pub struct ContiguousDiskSpaceIter<'a> {
+    input: &'a [u8],
+    cur_index: usize,
+}
+
+impl<'a> ContiguousDiskSpaceIter<'a> {
+    pub fn new(input: &'a [u8]) -> Self {
+        Self {
+            input,
+            cur_index: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for ContiguousDiskSpaceIter<'a> {
+    type Item = ContiguousDiskSpace;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let length = loop {
+            let length = *self.input.get(self.cur_index)? as usize;
+            if length != 0 {
+                break length;
+            }
+            self.cur_index += 1;
+        };
+        if self.cur_index % 2 == 0 {
+            let result = ContiguousDiskSpace::File(DiskFile {
+                id: self.cur_index / 2,
+                length,
+            });
+            self.cur_index += 1;
+            return Some(result);
+        }
+
+        let mut length = length;
+        loop {
+            self.cur_index += 1;
+            if let Some(&0) = self.input.get(self.cur_index) {
+                self.cur_index += 1;
+                if let Some(&next_len) = self.input.get(self.cur_index) {
+                    length += next_len as usize;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        Some(ContiguousDiskSpace::EmptySpace(EmptySpace { length }))
+    }
+}
--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,4 +1,8 @@
-use std::iter::repeat_n;
+mod contiguous_space_iter;
+
+use std::{iter::repeat_n, mem};
+
+use contiguous_space_iter::{ContiguousDiskSpace, ContiguousDiskSpaceIter, DiskFile, EmptySpace};
 
 fn parse_input(input: &[u8]) -> Vec<u8> {
     input
@@ -65,11 +69,168 @@ fn part1(input: &[u8]) -> usize {
         .sum()
 }
 
+fn combine_empties(contiguous_disk_spaces: &mut Vec<ContiguousDiskSpace>, i: usize) -> usize {
+    // given: contiguous_disk_spaces[i] is empty.
+    // combine empties of contiguous_disk_spaces[i - 1], contiguous_disk_spaces[i],
+    // contiguous_disk_spaces[i + 1]
+
+    // return the next index after original i and after the removal (either i or i + 1)
+
+    let ContiguousDiskSpace::EmptySpace(EmptySpace { length: cur_length }) =
+        contiguous_disk_spaces[i]
+    else {
+        panic!()
+    };
+
+    let maybe_next_elem = contiguous_disk_spaces.get(i + 1);
+    let maybe_next_length = maybe_next_elem
+        .map(|elem| match elem {
+            &ContiguousDiskSpace::EmptySpace(EmptySpace { length }) => Some(length),
+            _ => None,
+        })
+        .flatten();
+
+    if let Some(ContiguousDiskSpace::EmptySpace(EmptySpace {
+        length: prev_length,
+    })) = i
+        .checked_sub(1)
+        .map(|prev_index| contiguous_disk_spaces.get_mut(prev_index))
+        .flatten()
+    {
+        *prev_length += cur_length;
+        match maybe_next_length {
+            Some(next_length) => {
+                *prev_length += next_length;
+                contiguous_disk_spaces.drain(i..=i + 1);
+            }
+            None => {
+                contiguous_disk_spaces.remove(i);
+            }
+        }
+        return i;
+    }
+
+    match maybe_next_length {
+        Some(next_length) => {
+            let ContiguousDiskSpace::EmptySpace(EmptySpace { length: cur_length }) =
+                &mut contiguous_disk_spaces[i]
+            else {
+                panic!()
+            };
+
+            *cur_length += next_length;
+            contiguous_disk_spaces.remove(i + 1);
+        }
+        None => (),
+    }
+
+    i + 1
+}
+
+fn part2(input: &[u8]) -> usize {
+    let mut contiguous_disk_spaces = ContiguousDiskSpaceIter::new(input).collect::<Vec<_>>();
+    contiguous_disk_spaces.reverse();
+
+    let mut i = 0;
+    let final_file = contiguous_disk_spaces
+        .iter()
+        .filter_map(|space| {
+            if let ContiguousDiskSpace::File(disk_file) = space {
+                Some(disk_file)
+            } else {
+                None
+            }
+        })
+        .next();
+
+    let mut prev_id = match final_file {
+        Some(&DiskFile { id, .. }) => id,
+        None => return 0,
+    };
+
+    while i < contiguous_disk_spaces.len() {
+        let ContiguousDiskSpace::File(disk_file) = &contiguous_disk_spaces[i] else {
+            i += 1;
+            continue;
+        };
+
+        // already moved this file
+        if disk_file.id > prev_id {
+            i += 1;
+            continue;
+        }
+
+        prev_id = disk_file.id;
+
+        let Some((j, empty_space)) = contiguous_disk_spaces
+            .iter()
+            .enumerate()
+            .rev()
+            .take_while(|&(j, _)| j > i)
+            .filter_map(|(j, space)| {
+                if let ContiguousDiskSpace::EmptySpace(empty_space) = space {
+                    if empty_space.length >= disk_file.length {
+                        return Some((j, empty_space));
+                    }
+                }
+                None
+            })
+            .next()
+        else {
+            i += 1;
+            continue;
+        };
+
+        if empty_space.length == disk_file.length {
+            contiguous_disk_spaces.swap(i, j);
+            i = combine_empties(&mut contiguous_disk_spaces, i);
+        } else {
+            let disk_file_length = disk_file.length;
+
+            // todo insert it in j + 1 and consolidate prev and next empty spaces
+            let disk_file_to_insert = mem::replace(
+                &mut contiguous_disk_spaces[i],
+                ContiguousDiskSpace::EmptySpace(EmptySpace {
+                    length: disk_file_length,
+                }),
+            );
+            let ContiguousDiskSpace::EmptySpace(e) = &mut contiguous_disk_spaces[j] else {
+                panic!()
+            };
+            e.length -= disk_file_length;
+            contiguous_disk_spaces.insert(j + 1, disk_file_to_insert);
+            i = combine_empties(&mut contiguous_disk_spaces, i);
+        }
+    }
+
+    contiguous_disk_spaces.reverse();
+
+    let mut cur_index = 0;
+    contiguous_disk_spaces
+        .into_iter()
+        .filter_map(|contiguous_disk_space| {
+            match contiguous_disk_space {
+                ContiguousDiskSpace::File(DiskFile { id, length }) => {
+                    // sum of cur_index * id + (cur_index + 1) * id + ... + (cur_index + length - 1) * id
+                    let result = (id * length * (cur_index * 2 + length - 1)) / 2;
+                    cur_index += length;
+                    Some(result)
+                }
+                ContiguousDiskSpace::EmptySpace(EmptySpace { length }) => {
+                    cur_index += length;
+                    None
+                }
+            }
+        })
+        .sum()
+}
+
 fn main() {
     let file_contents = include_bytes!("../input.txt");
     let input = parse_input(file_contents);
 
     println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
 
 #[cfg(test)]
@@ -88,5 +249,11 @@ mod tests {
     fn test_part1() {
         let input = parse_input(TEST_INPUT);
         assert_eq!(1928, part1(&input))
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = parse_input(TEST_INPUT);
+        assert_eq!(2858, part2(&input))
     }
 }
