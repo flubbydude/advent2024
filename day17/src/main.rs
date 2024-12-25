8e2a21d0da400fd6ab1a68dfbcd5458e5af0a5c8
Stuart Lustig <stueyrama@gmail.com>
2024-12-24 20:17:56 -0500

    part 2 wip

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,6 +1,8 @@
 mod computer;
 mod puzzle_input;
 
+use std::iter;
+
 use itertools::Itertools;
 
 use computer::run_program;
@@ -10,6 +12,32 @@ fn part1(input: &PuzzleInput) -> String {
     run_program(&input.tribit_code, input.memory.clone())
         .into_iter()
         .join(",")
+}
+
+fn part1_decompiled(input: &PuzzleInput) -> String {
+    let mut a = input.memory.registers.a;
+    if a == 0 {
+        return "0".to_string();
+    }
+
+    iter::from_fn(|| {
+        if a == 0 {
+            return None;
+        }
+
+        let mut b = (a % 8) as u8;
+        b ^= 5;
+
+        let c = a.checked_shr(b as u32).unwrap_or_default();
+
+        a >>= 3;
+
+        b ^= (c % 8) as u8;
+        b ^= 6;
+
+        Some(b)
+    })
+    .join(",")
 }
 
 fn check_part2(input: &PuzzleInput, answer: u64) {
@@ -22,13 +50,27 @@ fn check_part2(input: &PuzzleInput, answer: u64) {
     assert_eq!(*input.tribit_code, *output);
 }
 
+fn part2(input: &PuzzleInput) -> u64 {
+    let mut a = 0;
+    for &tribit in input.tribit_code.iter().rev() {
+        let b0 = tribit ^ 0b110 ^ 0b101;
+
+        a |= b0 as u64;
+        a *= 8;
+    }
+    let answer = a;
+    check_part2(input, answer);
+    answer
+}
+
 fn main() {
     let input_str = include_str!("../input.txt");
 
     let input = PuzzleInput::parse_input(input_str);
 
     println!("{}", part1(&input));
-    check_part2(&input, 5);
+    println!("{}", part1_decompiled(&input));
+    println!("{}", part2(&input));
 }
 
 #[cfg(test)]
@@ -45,8 +87,22 @@ mod tests {
     }
 
     #[test]
-    fn test_check_part2() {
+    fn test_part1_decompiled() {
+        let input = PuzzleInput::parse_input(TEST_INPUT);
+        assert_eq!("4,6,3,5,6,3,5,2,1,0", part1_decompiled(&input))
+    }
+
+    #[test]
+    fn part2_example() {
+        const PART2_EXAMPLE_SOLN: u64 = 117440;
+
         let input = PuzzleInput::parse_input(TEST_INPUT_PART2);
-        check_part2(&input, 117440);
+        let mut a = 0;
+        for &tribit in input.tribit_code.iter().rev() {
+            a |= tribit as u64;
+            a <<= 3;
+        }
+        assert_eq!(a, PART2_EXAMPLE_SOLN);
+        check_part2(&input, PART2_EXAMPLE_SOLN);
     }
 }
