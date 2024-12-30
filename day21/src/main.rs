eecc364d797b21cba3d24f6b72ca26dff97b3c92
Stuart Lustig <stueyrama@gmail.com>
2024-12-30 10:51:40 -0800

    wip with comment

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -8,7 +8,7 @@ use std::ops::Deref;
 use array2d::Array2D;
 
 use instruction::Instruction;
-use keypad::{build_keypad_from_string, KeypadShortestPaths};
+use keypad::build_keypad_from_string;
 use numeric_keypad_button::NumericKeypadButton;
 
 const NUMERIC_KEYPAD_STR: &str = include_str!("../numeric_keypad.txt");
@@ -40,35 +40,37 @@ fn get_button_position<T: Eq>(button_to_find: &T, keypad: &Array2D<Option<T>>) -
         .0
 }
 
-// return length of shortest sequence
-fn part1_helper(keypad_code: &[NumericKeypadButton]) -> usize {
+fn shortest_sequence_length(keypad_code: &[NumericKeypadButton]) -> usize {
     let instruction_keypad_str = build_keypad_from_string::<Instruction>(INSTRUCTION_KEYPAD_STR);
-    let instruction_keypad_shortest_paths =
-        KeypadShortestPaths::new_from_keypad(&instruction_keypad_str);
 
     let numeric_keypad = build_keypad_from_string::<NumericKeypadButton>(NUMERIC_KEYPAD_STR);
-    let numeric_keypad_shortest_paths = KeypadShortestPaths::new_from_keypad(&numeric_keypad);
 
-    let mut my_position = get_button_position(&Instruction::Activate, &instruction_keypad_str);
-    let mut numeric_robot_position =
-        get_button_position(&NumericKeypadButton::Activate, &numeric_keypad);
-    let mut robot1_position = my_position;
-    let mut robot2_position = my_position;
-
-    let mut my_sequence_length = 0;
-
-    for number in keypad_code {
-        let numeric_robot_target = 
-        numeric_keypad_shortest_paths.get_shortest_paths(source, target)
-    }
-
+    // todo rethink this as search
+    // the code needs to store state as the robot positions along with
+    // what has been output so far, and one step is going to be a movement from my_position
+    // which will affect the downstream robots as needed.
+    // Also store the path at each step
+    //
+    // Prune branches if the code entered is incorrect
+    // Also prune branches if any robot goes off grid
+    //
+    // Prune branches based on stored path length at that state if seen (HashMap<State, Vec<Instruction>>)
+    // Also can prune branches if number robot veers off of an ideal path, like if the numeric robot goes
+    //    in a direction away from the next key (ex: need to go from 9 to A but went left)
+    //    - Could use heuristic as distance numeric robot hand to the next button needed?!
+    //    - If numeric robot moves in a direction, make sure the heuristic goes down
+    //
+    // Quite optional since idk if easily possible, prune branch if a robot went back the way it just came or off
+    // back on a path since the last press. Would cover the "number robot veers off of an ideal path"
+    //
+    // Much simpler!
     todo!()
 }
 
 fn part1<A: Deref<Target = [NumericKeypadButton]>>(keypad_codes: &[A]) -> usize {
     keypad_codes
         .iter()
-        .map(|code| part1_helper(code) * get_numeric_part_of_code(code))
+        .map(|code| shortest_sequence_length(code) * get_numeric_part_of_code(code))
         .sum()
 }
 
