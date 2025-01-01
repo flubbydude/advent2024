5be02c305f8fd8c938a017ea165f8acbb5aa60fd
Stuart Lustig <stueyrama@gmail.com>
2025-01-01 18:27:38 -0500

    new plan for day21

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -63,6 +63,14 @@ fn part1(keypad_codes: &[&[u8]]) -> usize {
         .sum()
 }
 
+// what if:
+// Get all the best paths for numeric robot
+// Then from that get all the best paths for normal robot N - 1
+// Some will be pruned due to taking longer steps
+// From that, get all the best paths for normal robot N - 2
+// ...
+// Get all/1 of the best paths for the main user guy
+// return the length.
 fn part2(keypad_codes: &[&[u8]]) -> usize {
     const NUM_INTERMEDIATE_ROBOTS: usize = 25;
     keypad_codes
